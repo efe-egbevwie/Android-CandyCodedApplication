084f6cce71d0b3f2d73cecbcd3f4cf1885d78cff
[Efe Egbevwie] <[efe1705@gmail.com]>
2020-09-25 19:34:44 +0100

    created shareIntent

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -80,9 +80,10 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void createShareIntent() {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
         String share = SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED ;
-        intent.putExtra(Intent.EXTRA_TEXT, share);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, share);
+        startActivity(shareIntent);
     }
 }
